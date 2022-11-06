4e08bfd686d4d3dc21cfd6730704a8985f2cc9d8
Abeer Sobhy <87787045+abeersobhy@users.noreply.github.com>
2022-11-06 13:22:02 +0200

    Add files via upload

---
+++ I2C/I2C.c
@@ -0,0 +1,120 @@
+﻿/*
+ * I2C.c
+ *
+ * Created: 10/10/2022 07:26:36 م
+ *  Author: user
+ */ 
+#define F_CPU 16000000UL
+#include "../../Utilities/std_types.h"
+#include "../../Utilities/bit_math.h"
+#include "../../Utilities/register.h"
+#include <math.h>
+#include "I2C.h"
+
+I2C_Configuration I2C;
+void I2C_Init()
+{	
+	/*TWBR = ((F_CPU / SCL_frequency) - 16) / (2*(4^prescalerValue)))*/
+	switch(I2C.ControlState)
+	{
+		case I2C_MasterEnable :
+		     switch(I2C.prescaler)
+		     {
+			     case I2C_Prescaler1 :
+			          TWBR = ((F_CPU / I2C.SCL_Frequency) - 16) / (2*1);
+			     break;
+			     case I2C_Prescaler4:
+			          TWBR = ((F_CPU / I2C.SCL_Frequency) - 16) / (2*pow(4 , 4));
+			     break;
+			     case I2C_Prescaler16:
+			          TWBR = ((F_CPU / I2C.SCL_Frequency) - 16) / (2*pow(4 , 16));
+			     break;
+			     case I2C_Prescaler64:
+			          TWBR = ((F_CPU / I2C.SCL_Frequency) - 16) / (2*pow(4 , 64));
+			     break;
+			     default:
+			     break;
+		     }
+		break;
+		case I2C_SlaveEnable :
+		     TWAR |= (I2C.ID << 1);
+			 switch(I2C.acknowledgeState)
+			 {
+				 case I2C_AcknowledgeEnable :
+				 TWCR |= (1 << TWEA);	/*Enable Acknowledge bit.*/
+				 break;
+				 case I2C_AcknowledgeDisable: /*Nothing to do.*/
+				 break;
+				 default:
+				 break;
+			 }
+		break;
+		default:
+		break;	 		 	
+	}
+}
+
+void I2C_MasterStart()
+{
+	TWCR |= (1 << TWINT);	/*Start the operation of the I2C.*/
+	TWCR |= (1 << TWSTA);	/*Start bit.*/
+	switch(I2C.ControlState)
+	{
+		case I2C_MasterEnable:
+             TWCR |= (1 << TWEN);	/*Enable I2C.*/	
+		break;
+		case I2C_Disable : /*Nothing to do.*/
+		break;
+		default:
+		break;
+	}
+	switch(I2C.interruptControl)
+	{
+		case I2C_InterruptEnable :
+		     TWCR |= (1 << TWIE);	/*Enable Interrupt.*/	 
+		break;
+		case I2C_InterruptDisable : /*Nothing to do.*/
+		break;
+		default:
+		break;
+	}
+	while(READ_BIT(TWCR , TWINT) == 0); /*Wait until the hardware set the TWINT Pin*/ 		
+}
+void I2C_MasterAddress(uint8_t ID)
+{
+	TWDR = (ID << 1);
+	(I2C.RW == 1) ? SET_BIT(TWCR , TWWC) : CLR_BIT(TWCR , TWWC); 
+	TWCR |= (1 << TWINT) | (1 << TWEN);	/*Start the operation of the I2C.*/ 
+	while(READ_BIT(TWCR , TWINT) == 0); /*Wait until the hardware set the TWINT Pin*/
+}
+void I2C_MasterWrite(uint8_t data)
+{
+	TWDR = data; /*Data to send.*/
+	TWCR |= (1 << TWINT) | (1 << TWEN);	/*Start the operation of the I2C.*/
+	while(READ_BIT(TWCR , TWINT) == 0); /*Wait until the hardware set the TWINT Pin*/
+}
+uint8_t I2C_MasterRead()
+{
+	TWCR |= (1 << TWEA) | (1 << TWEN) | (1 << TWINT); /*Start the operation of the I2C.*/
+	while(READ_BIT(TWCR , TWINT) == 0);
+	return TWDR; /*Data is received.*/ 
+}
+void I2C_MasterStop()
+{
+	TWCR |= (1 << TWINT) | (1 << TWSTO)| (1 << TWEN);	/*Stop Condition.*/
+}
+uint8_t I2C_SlaveRead()
+{
+	TWCR |= (1 << TWINT) | (1 << TWEN) | (1 << TWEA); /*Start the operation of the I2C.*/
+	while(READ_BIT(TWCR , TWINT) == 0); /*Current event is not complete.*/
+	return TWDR; /*Data is received.*/
+}
+uint8_t I2C_slaveAvialable()
+{	
+	TWCR  = (1<<TWEN) | (1<<TWINT) |(1<<TWEA); /*Start the operation of the I2C.*/
+	while(READ_BIT ( TWCR , TWINT) == 0);
+	if(TWSR == 0x60) 
+	   return 1;
+	else 
+	   return 0;
+}
---
+++ I2C/I2C_Config.h
@@ -0,0 +1,31 @@
+﻿/*
+ * I2C_Config.h
+ *
+ * Created: 24/10/2022 09:15:50 م
+ *  Author: user
+ */ 
+
+
+#ifndef I2C_CONFIG_H_
+#define I2C_CONFIG_H_
+
+/*Status Codes for Master Transmitter Mode*/
+#define START_TRANSMITED                        0x08
+#define REPEATED_START_TRANSMITED               0x10
+#define SLA_W_TRANSMITTED_ACK_RECEIVED          0x18 
+#define SLA_W_TRANSMITTED_NOT_ACK_RECEIVED      0x20
+#define DATA_TRANSMITTED_ACK_RECEIVED           0x28
+#define DATA_TRANSMITTED_NOT_ACK_RECEIVED       0x30
+#define ARIBITRATION_LOST                       0x38   
+/*Status Codes for Master Receiver Mode*/
+#define START_TRANSMITED                        0x08
+#define ARIBITRATION_LOST                       0x38
+#define SLA_R_TRANSMITTED_ACK_RECEIVED          0x40
+#define SLA_R_TRANSMITTED_NOT_ACK_RECEIVED      0x48
+#define DATA_RECEIVED_ACK_RETURNED              0x50
+#define DATA_RECEIVED_NOT_ACK_RETURNED          0x58
+
+/*Status Codes for Slave Receiver Mode */
+
+/*Status Codes for Slave Transmitter Mode*/
+#endif /* I2C_CONFIG_H_ */
